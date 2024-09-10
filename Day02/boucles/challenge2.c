c1a5d7b8597b4c50902e1f88f35709d9f5d092d6
LatrachDev <m.latrach.youcode@gmail.com>
2024-09-10 16:21:12 +0200

    third push

--- Day02/boucles/challenge2.c
+++ Day02/boucles/challenge2.c
@@ -2,7 +2,25 @@
 
 int main()
 {
-    int x;
+    int n, x;
+    unsigned long long factorial = 1;
+
+    printf("Entrez un nombre entier positif : ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("La factorielle n'existe pas pour les nombres negatifs !\n");
+    }
+    else
+    {
+        for (x = 1; x <= n; x++)
+        {
+            factorial *= x;
+        }
+        
+    }
+    printf("%d! = %llu\n", n, factorial);
     
     return 0;
 }
